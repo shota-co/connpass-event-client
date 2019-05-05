55090dd480a2bc487daac2a3530489be314fba6a
shota-co <dripcoffee1018@gmail.com>
2019-05-05 16:14:35 +0900

    Updates

---
+++ precache-manifest.b33c85db5b2fa3818dd140935426ec48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a36702514c00a7083358e9264f401ddc",
+    "url": "/connpass-event-client/index.html"
+  },
+  {
+    "revision": "8dfb58c379c46e91d410",
+    "url": "/connpass-event-client/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "b5723d09c136565c3bb1",
+    "url": "/connpass-event-client/static/js/2.7593b0cf.chunk.js"
+  },
+  {
+    "revision": "8dfb58c379c46e91d410",
+    "url": "/connpass-event-client/static/js/main.bdfc4724.chunk.js"
+  },
+  {
+    "revision": "ea1e06659a4d06920641",
+    "url": "/connpass-event-client/static/js/runtime~main.b498b43d.js"
+  }
+]);
